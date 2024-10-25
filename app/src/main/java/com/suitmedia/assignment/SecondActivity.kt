9759ea29c92ed89b8807009394a8bd3ecec909cb
Hofta1 <c.collhins@gmail.com>
2024-10-25 11:43:11 +0700

    SuitmediaAssignmen 25/10/2024

--- app/src/main/java/com/suitmedia/assignment/SecondActivity.kt
+++ app/src/main/java/com/suitmedia/assignment/SecondActivity.kt
@@ -3,7 +3,6 @@ package com.suitmedia.assignment
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
